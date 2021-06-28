9eca004a90dcf4c0d86d9dfbacfedfa28c963c6c
Arpit Kushwaha <arpitkushwah01@gmail.com>
2021-06-29 02:49:28 +0530

    twoSum problem done

--- vector/problems.cpp
+++ vector/problems.cpp
@@ -2,49 +2,87 @@
 #include <string>
 #include <vector>
 #include <unordered_set>
-
+#include <unordered_map>
 using namespace std;
 
 //https://www.geeksforgeeks.org/modify-array-of-strings-by-replacing-characters-repeating-in-the-same-or-remaining-strings/
-void modifyArray()
+// void modifyArray()
+// {
+//     vector<string> a;
+//     vector<string> ans;
+//     unordered_set<char, int> flag;
+//     string str;
+//     int i=1;
+//     while(i)
+//     {
+//         cout<<"str: ";cin>>str;
+//         a.push_back(str);
+//         cout<<"i: ";cin>>i;
+//     }
+
+//     for(int i=0; i<a.size(); i++)
+//     {
+//         string temp;
+//         for(int j=0; j<a[i].size(); j++)
+//         {
+//             if(flag.find(a[i][j]) == flag.end())
+//                 {
+//                     flag.insert({a[i][j],1});
+//                     temp.append(string(1,a[i][j]));
+//                 }
+
+//         }        
+//         ans.push_back(temp);
+//     }
+
+//     for(vector<string>::iterator itr = ans.begin(); itr!=ans.end(); itr++)
+//     {
+//         cout<<*itr<<", ";
+//     }
+
+
+// }
+
+
+void twoSum()
 {
-    vector<string> a;
-    vector<string> ans;
-    unordered_set<char, int> flag;
-    string str;
-    int i=1;
-    while(i)
+    vector<int> a;
+    unordered_map<int, int> map;
+    int target = 0;
+    int e=1;
+    while(e)
     {
-        cout<<"str: ";cin>>str;
-        a.push_back(str);
-        cout<<"i: ";cin>>i;
+        cin>>e;
+        if(e==0)
+            break;
+        a.push_back(e);
     }
 
-    for(int i=0; i<a.size(); i++)
+    cout<<"Target: "; cin>>target;
+
+    for(int &first_no: a)
     {
-        string temp;
-        for(int j=0; j<a[i].size(); j++)
+        int second_no = target - first_no;
+        if(map.find(second_no) != map.end())
         {
-            if(flag.find(a[i][j]) == flag.end())
-                {
-                    flag.insert({a[i][j],1});
-                    temp.append(string(1,a[i][j]));
-                }
-
-        }        
-        ans.push_back(temp);
+            cout<<"Found : "<<first_no<<", "<<second_no; 
+            return;
+        }
+        map[first_no]++;
     }
 
-    for(vector<string>::iterator itr = ans.begin(); itr!=ans.end(); itr++)
-    {
-        cout<<*itr<<", ";
-    }
-
-
+    cout<<"Not Found"<<endl;
 }
+
+
+void threeSum()
+{
+    
+}
+
 
 int main()
 {
-    modifyArray();
+    twoSum();
     return 0;
 }
